7810535e4fae437e9aa222859e853bd7bed4d655
APSN4 <tema.sergeevich2012@yandex.ru>
2025-01-19 03:43:58 -0800

    feature: hanoi_tower

--- recursion_algorithms/main.py
+++ recursion_algorithms/main.py
@@ -1,4 +1,21 @@
 count_disks = int(input())
+count_iterations = 0
+iterations = []
 
-def hanoi_tower(count):
-    pass
+def hanoi_tower(count, fromPin, toPin):
+    global count_iterations
+    if count == 1:
+        iterations.append(f'{fromPin} {toPin}')
+        count_iterations += 1
+        return
+    unusedPin = 6 - fromPin - toPin
+    hanoi_tower(count - 1, fromPin, unusedPin)
+    iterations.append(f'{fromPin} {toPin}')
+    count_iterations += 1
+    hanoi_tower(count - 1, unusedPin, toPin)
+
+hanoi_tower(count_disks, 1, 3)
+
+print(count_iterations)
+for msg in iterations:
+    print(msg)
