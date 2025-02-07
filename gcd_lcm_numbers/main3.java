3ecf3f9adfee7d2527dc30bba5800d108266f00f
APSN4 <tema.sergeevich2012@yandex.ru>
2025-02-07 08:30:14 -0800

    gcd_lcm_numbers/main3

---
+++ gcd_lcm_numbers/main3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public long[] fibonacci(long n) {
+        long last = 0;
+        long current = 1;
+        for (int i = 0; i < n; i++) {
+            long temp = last + current;
+            if (temp > n) {
+                break;
+            }
+            last = current;
+            current = temp;
+        }
+        return new long[]{last, current};
+    }
+
+    public static void main(String[] args) {
+        Main mainInstance = new Main();
+         Scanner reader = new Scanner(System.in);
+         long number = reader.nextLong();
+         long[] output = mainInstance.fibonacci(number);
+        System.out.println(output[0] + " " + output[1]);
+    }
+}
