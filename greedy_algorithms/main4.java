57adeadb9f2964c170167c3ae1e2657e3adbea84
APSN4 <tema.sergeevich2012@yandex.ru>
2025-02-12 12:28:49 -0800

    feature: greedy_algorithms/main4

---
+++ greedy_algorithms/main4.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+
+    public double robberMaxValue(ExersiceInput exersiceInput) {
+        ArrayList<ArrayList<Double>> valueProducts = new ArrayList<>();
+
+        for (ArrayList<Integer> arr: exersiceInput.products) {
+            double value = arr.get(0) * ((double) 1 / arr.get(1));
+            valueProducts.add(new ArrayList<>(Arrays.asList(value, arr.get(1).doubleValue())));
+        }
+
+        valueProducts.sort((a, b) -> Double.compare(b.get(0), a.get(0)));
+
+        double maxValue = 0;
+        for (ArrayList<Double> product : valueProducts) {
+            if (product.get(1) > exersiceInput.volumeBackpack && exersiceInput.volumeBackpack != 0) {
+                maxValue += product.get(0) * exersiceInput.volumeBackpack;
+                exersiceInput.volumeBackpack = 0;
+            } else if (exersiceInput.volumeBackpack != 0 && product.get(1) <= exersiceInput.volumeBackpack) {
+                maxValue += product.get(0) * product.get(1);
+                exersiceInput.volumeBackpack -= product.get(1);
+            }
+        }
+        return maxValue;
+    }
+
+    public static void main(String[] args) {
+        ExersiceInput exersiceInput = new ExersiceInput();
+        Scanner scanner = new Scanner(System.in);
+        exersiceInput.count = scanner.nextInt();
+        exersiceInput.volumeBackpack = scanner.nextInt();
+
+        for (int i = 0; i < exersiceInput.count; i++) {
+            exersiceInput.products.add(new ArrayList<>(Arrays.asList(scanner.nextInt(), scanner.nextInt())));
+        }
+
+        Main main = new Main();
+        String output = String.valueOf(Math.floor(main.robberMaxValue(exersiceInput) * 1000 +.5) / 1000);
+        if (output.endsWith(".0")) {
+            System.out.println(output + "00");
+        } else {
+            System.out.println(output);
+        }
+    }
+}
+
+class ExersiceInput {
+    public int count;
+    public int volumeBackpack;
+    public ArrayList<ArrayList<Integer>> products = new ArrayList<>();
+}
